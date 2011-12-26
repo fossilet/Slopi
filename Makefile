1e37d7e262a8df4387e22be0954870b8d43e3b0f
Yongzhi Pan <panyongzhi@gmail.com>
2011-12-26 20:34:14 +0800

    Better error handling.

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ clean: slopi itoa
 	rm slopi itoa
 
 itoa: itoa.c
-	gcc -o itoa itoa.c modp_numtoa.o
+	gcc -o itoa itoa.c modp_numtoa_mod.c
 slopi: slopi.c Makefile
 	gcc -Ofast -march=core2 -Wno-unused-result -o slopi slopi.c modp_numtoa.c -lm -pthread
--- slopi.c
+++ slopi.c
@@ -1,5 +1,4 @@
 /* Find self-locating numbers in numbers, esp. in Pi.
- * For speed consideration, not all call errors are handles.
  * modp_numtoa is from stringencoders at 
  *     https://code.google.com/p/stringencoders/wiki/NumToA
  *
@@ -21,50 +20,55 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#if LONG_MAX < 1000000000000
+#if LONG_MAX <= 1000000000000
 #error Your compiler`s long int type is too small!
 #endif
 
-#define OFFSET 2 // How many bytes to bypass. For 3.1415... OFFSET is 2.
-#define err_exit(en, msg) \
+#define OFFSET 2 // Number of bytes to bypass. For 3.1415... OFFSET is 2.
+#define err_exit_en(en, msg) \
     do { errno=en; perror(msg); exit(EXIT_FAILURE); } while (0)
+#define err_exit(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while(0)
 
 static void *find(void *file) {
+    int s;
     // set CPU affinity
     // FIXME fixed two threads onto the same core, slows down.
     /*
-    int s;
     cpu_set_t set;
     CPU_ZERO(&set);
     CPU_SET(1, &set);
     s = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &set);
     if(s != 0)
-        err_exit(s, "pthread_setaffinity_np");
+        err_exit_en(s, "pthread_setaffinity_np");
         */
 
     char *infile = (char *)file;
     struct stat stat_buf;
     int fd;
-    long N;
+    off_t N;
+    off_t n = 1;
     int num_read;
+    int maxlen = (int)ceil(log10f((float)N));
+    char buf[2*maxlen];
+    //printf("maxlen: %d\n", maxlen);
+    int len = 1;
+    
 
     stat(infile, &stat_buf);
     N = stat_buf.st_size;
     fd = open(infile, O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "File %s open error!\n", infile);
-        exit(1);
-    }
+    if (fd == -1) 
+        err_exit("open");
+    s = posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
+    if(s != 0)
+        err_exit_en(s, "posix_fadvise");
 
-    int maxlen = (int)ceil(log10f((float)N));
-    char buf[2*maxlen];
-    //printf("maxlen: %d\n", maxlen);
-    long n = 1;
-    int len = 1;
     sprintf(buf, "%d", 1);
-    // FIXME detect error
-    lseek(fd, OFFSET, SEEK_SET);
+    if (lseek(fd, OFFSET, SEEK_SET) == (off_t)-1)
+        err_exit("lseek");
     do {
+        // For speed consideration, not all call errors are handled.
         num_read = read(fd, buf+len, len);
         //printf("buf: %.*s, len: %d, n: %d\n", 2*len, buf, len, n);
         if(!memcmp(buf, buf+len, len))
@@ -72,16 +76,20 @@ static void *find(void *file) {
                     len, buf, (unsigned long)(pthread_self()));
         modp_litoa10(n+1, buf);
         len = strlen(buf);
-    } while((n=lseek(fd, n+OFFSET, SEEK_SET)-OFFSET+1) < N);
-    //   ^ off_t is signed int by SUSv3 type requirement
+    } while( (n=lseek(fd, n+OFFSET, SEEK_SET)-OFFSET+1) < N );
 }
 
 int main(int argc, char *argv[]) {
-    // FIXME detect error
     long num_cpu =  sysconf(_SC_NPROCESSORS_ONLN);
+    if (num_cpu == -1)
+       err_exit("sysconf"); 
     printf("CPU number: %ld\n", num_cpu);
     //char *files[num_cpu];
     const char * const files[2] = {"xac", "xad"};
+    pthread_t tid[num_cpu];
+    int i;
+    //int j;
+    int s;
 
     if(num_cpu == 2) {
         // FIXME initialize once
@@ -91,32 +99,24 @@ int main(int argc, char *argv[]) {
         ;
     }
 
-    //char *files[argc-1][num_cpu];
-    pthread_t tid[num_cpu];
-    int i;
-    //int j;
-    int error;
-/*
+    /*
     for(j=0; j<argc-1; j++) {
         files[j][0] = argv[2*j+1];
         files[j][1] = argv[2*j+2];
     }
     */
-
     for(i=0; i<num_cpu; i++) {
-        error = pthread_create(&tid[i], NULL, find, (void *)files[i]);
-        if(error)
-            fprintf(stderr, "Couln't run thread No. %d, errno %d\n",
-                    i, error);
+        s = pthread_create(&tid[i], NULL, find, (void *)files[i]);
+        if(s != 0)
+            err_exit_en(s, "pthread_create");
     }
 
     /* now wait for all threads to terminate */
-    for(i=0; i<num_cpu; i++)
-        // FIXME detect error
-        pthread_join(tid[i], NULL);
+    for(i=0; i<num_cpu; i++) {
+        s = pthread_join(tid[i], NULL);
+        if(s != 0)
+            err_exit_en(s, "pthread_join");
+    }
 
-    //printf("INT_MAX: %d, LONG_MAX: %ld, LLONG_MAX, %lld\n", INT_MAX, LONG_MAX, LLONG_MAX);
-    printf("int32_t: %lu, int64_t: %lu\n", sizeof(int32_t), sizeof(int64_t));
-    printf("off_t: %lu, off64_t: %lu\n", sizeof(off_t), sizeof(off64_t));
     return 0;
 }
